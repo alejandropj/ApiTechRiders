d987dfc71ec5fbce89b3848cb65155c2b37e6f89
alejandropj <alejandroparrajimenez@gmail.com>
2024-05-09 20:16:41 +0200

    Contacto con el administrador v1

---
+++ ApiTechRiders/Models/Email.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiTechRiders.Models
+{
+    public class Email
+    {
+        public string Asunto { get; set; }
+        public string Comentario { get; set; }
+    }
+}
